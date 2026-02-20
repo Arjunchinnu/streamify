ef0f35580c5e7af604ac3d41143c8e237a5ba8ec
arjunchinnu604@gmail.com <user.email like : "arjunchinnu604@gmail.com>
2026-02-20 17:47:16 +0530

    connecting backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://streamify-backend-0vd4.onrender.com/api";
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
